7f2c7ca3695e509b4873e4a4361dd918d70dc0a7
Seoyong Nam <namsyong0508@hanyang.ac.kr>
2023-10-16 22:59:16 +0900

    first commit

---
+++ apps/crud/views.py
@@ -0,0 +1,23 @@
+from apps.app import db
+from apps.crud.models import User
+
+from flask import Blueprint, render_template
+
+
+crud = Blueprint(
+    "crud",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+)
+
+
+@crud.route("/")
+def index():
+    return render_template("crud/index.html")
+
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "콘솔 로그를 확인해 주세요"
